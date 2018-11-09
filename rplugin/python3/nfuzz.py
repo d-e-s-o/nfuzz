117b83dfab3f3f5ce0bb44f41a43c112a428c225
Daniel Mueller <deso@posteo.net>
2018-11-08 20:47:10 -0800

    Add fuzzy file open support
    While opening buffers from a list may be the more common operation,
    sometimes it is necessary to open a file that has not been opened
    before.
    This change adds support for doing so. We try to find the root directory
    of the source tree we are working on, represented as the directory
    common to all already opened files. We then use 'fd' to list all files
    below that directory and filter them in a fuzzy manner using fzy.

--- rplugin/python3/nfuzz.py
+++ rplugin/python3/nfuzz.py
@@ -20,6 +20,13 @@
 from itertools import (
   islice,
 )
+from os.path import (
+  commonpath,
+  dirname,
+  expanduser,
+  isabs,
+  join,
+)
 from neovim import (
   function,
   plugin,
@@ -30,6 +37,8 @@ from re import (
 from subprocess import (
   CalledProcessError,
   check_output,
+  PIPE,
+  Popen,
 )
 
 
@@ -45,9 +54,8 @@ class Main(object):
     self.vim = vim
 
 
-  @function("NfuzzBuffers", sync=False)
-  def buffers(self, args):
-    """Select a buffer to open by using 'fzy' on the output of Nvim's 'ls'."""
+  def iterBuffers(self):
+    """Retrieve an iterator over all the available buffers."""
     def filterBufs(line):
       """Filter out the buffer names from the output of 'ls'."""
       m = Main.LS_REGEX.match(line)
@@ -59,12 +67,66 @@ class Main(object):
     buffers = islice(buffers, 1, None)
     # Then extract the actual buffer names.
     buffers = map(filterBufs, buffers)
-    buffers = map(lambda x: x.encode(), buffers)
-    buffers = b"\n".join(buffers)
+    return buffers
 
+
+  @function("NfuzzBuffers", sync=False)
+  def buffers(self, args):
+    """Select a buffer to open by using 'fzy' on the output of Nvim's 'ls'."""
+    buffers = map(lambda x: x.encode(), self.iterBuffers())
+    buffers = b"\n".join(buffers)
     try:
       out = check_output(["fzy-tmux"], input=buffers)
     except CalledProcessError as e:
       self.vim.command("echo \"%s\"" % str(e))
     else:
       self.vim.command("buffer %s" % out.decode())
+
+
+  def cwd(self):
+    """Retrieve the current working directory Nvim is in."""
+    return self.vim.command_output("pwd").strip()
+
+
+  def root(self):
+    """Retrieve the directory root of all opened files.
+
+      Neovim generally supports two modes of operation: One in which the
+      current working directory is fixed and another one in which the
+      editor always changes to the directory containing the file being
+      edited. The more general approach is to be able to handle the
+      second case and so that is what this function attempts to do.
+
+      In a nutshell, we take the absolute paths to all already open
+      files (i.e., buffers) and find the common prefix they all share.
+    """
+    def mkabs(path):
+      """Make a path an absolute path."""
+      # Buffer names may abbreviate the user's home directory, so make
+      # sure to expand it.
+      path = expanduser(path)
+
+      if not isabs(path):
+        path = join(cwd, path)
+
+      return path
+
+    cwd = self.cwd()
+    paths = list(map(mkabs, self.iterBuffers()))
+    if len(paths) == 1:
+      return dirname(paths[0])
+    else:
+      return commonpath(list(paths))
+
+
+  @function("NfuzzFiles", sync=False)
+  def files(self, args):
+    """Select a file to open by using 'fzy' on the files below the source root directory."""
+    try:
+      p1 = Popen(["fd", "--type=f", ".", self.root()], stdout=PIPE)
+      p2 = Popen(["fzy-tmux"], stdin=p1.stdout, stdout=PIPE)
+      out, _ = p2.communicate()
+    except CalledProcessError as e:
+      self.vim.command("echo \"%s\"" % str(e))
+    else:
+      self.vim.command("edit %s" % out.decode())
